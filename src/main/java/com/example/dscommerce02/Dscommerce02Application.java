362da6e89fb5a45ae9957f1bd11a92e1fa97eb21
helderargomes <helderargomes@gmail.com>
2026-04-04 18:11:31 -0300

    BCrypt password

--- src/main/java/com/example/dscommerce02/Dscommerce02Application.java
+++ src/main/java/com/example/dscommerce02/Dscommerce02Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Dscommerce02Application {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(Dscommerce02Application.class, args);
